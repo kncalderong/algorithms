51a6332f7f16768fbd82b97f3e809cad570e16db
Kevin Nicolas <kncalderong@gmail.com>
2023-06-03 20:02:55 -0500

    feat: traverse the tree by levels included

--- dataStructures.js
+++ dataStructures.js
@@ -514,6 +514,46 @@ function BinarySearchTree() {
     return values
   }
   
+  ///to traverse root by levels: 
+  function traverse(direction, root) {
+    const queue = [root];
+    const results = [];
+
+    function pushIfThere(node, queue) {
+      if (node) queue.push(node);
+    }
+
+    while (queue.length > 0) {
+      const node = queue.shift();
+      results.push(node.value);
+      if (direction === "level") {
+        // Level order: left to right
+        pushIfThere(node.left, queue);
+        pushIfThere(node.right, queue);
+      } else {
+        // Reverse level order: right to left
+        pushIfThere(node.right, queue);
+        pushIfThere(node.left, queue);
+      }
+    }
+
+    return results;
+  }
+
+  // Level order traversal
+  this.levelOrder = function() {
+    if (!this.root) return null;
+
+    return traverse("level", this.root);
+  }
+
+  // Reverse level order traversal
+  this.reverseLevelOrder = function() {
+    if (!this.root) return null;
+
+    return traverse("reverseLevel", this.root);
+  }
+  
 }
 
 function isBinarySearchTree(tree) {
